ff66454fa1c57d379a753fc4648104abffd0cfc4
Grom <93106270+lGrom@users.noreply.github.com>
2023-06-13 22:28:10 -0500

    installed discordjs and eslint

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const perspective = require('./perspective');
 
 (async function() {
-    console.log( await perspective.getScores("im going to kill you!", ["TOXICITY", "PROFANITY"]) )
+	console.log(await perspective.getScores('im going to kill you!', ['TOXICITY', 'PROFANITY']));
 })();
--- perspective.js
+++ perspective.js
@@ -1,45 +1,52 @@
-require('dotenv').config()
+/* eslint-disable no-mixed-spaces-and-tabs */
+require('dotenv').config();
 const axios = require('axios').default;
 
 /**
- * Gets scores for the supplied attributes 
+ * Gets scores for the supplied attributes
  * @param {string} message - The message to analyze
  * @param {array} attributes - What to analyze the message for
  * @returns {Promise} The attributes requested and their scores
  */
-async function getScores (message, attributes) {
-    try {
+async function getScores(message, attributes) {
+	try {
 
-        const raw = await axios.post(`https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze?key=${process.env.PERSPECTIVE_KEY}`, {
-    	    "comment": {
-	        	"text": message,
+		const raw = await axios.post(`https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze?key=${process.env.PERSPECTIVE_KEY}`, {
+			'comment': {
+	        	'text': message,
 	        },
-    	    "languages": ["en"],
-	        "requestedAttributes": formatAttributes(attributes),
-            }, { "Content-Type": 'application/json' }
-        );
+    	    'languages': ['en'],
+	        'requestedAttributes': formatAttributes(attributes),
+		}, { 'Content-Type': 'application/json' },
+		);
 
-        let out = {};
+		const out = {};
 
-        for ( const attribute of attributes ) {
-            out[attribute] = raw.data.attributeScores[attribute].summaryScore.value;
-        }
+		for (const attribute of attributes) {
+			out[attribute] = raw.data.attributeScores[attribute].summaryScore.value;
+		}
 
-        return out;
+		return out;
 
-    } catch (error) {
-        console.log(error);
-    }
+	}
+	catch (error) {
+		console.log(error);
+	}
 }
 
-function formatAttributes (attributes) {
-    let formattedAttributes = {}
+/**
+ * Formats attributes to function with perspective's api
+ * @param {array} attributes - The attributes desired
+ * @returns {object} - The attributes in their correct format
+ */
+function formatAttributes(attributes) {
+	const formattedAttributes = {};
 
-    attributes.forEach((attribute) => {
-        formattedAttributes[attribute] = {}
-    })
+	attributes.forEach((attribute) => {
+		formattedAttributes[attribute] = {};
+	});
 
-    return formattedAttributes;
+	return formattedAttributes;
 }
 
 exports.getScores = getScores;
