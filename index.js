4ac8abe5c6f8cc682bd48595cd4dbe5606a824da
Grom <93106270+lGrom@users.noreply.github.com>
2023-06-14 22:34:31 -0500

    added event handling

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
 require('dotenv').config();
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -30,31 +30,18 @@ for (const folder of commandFolders) {
 	}
 }
 
-client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-	const command = interaction.client.commands.get(interaction.commandName);
-
-	if (!command) {
-		console.error(`[WARNING] No command matching ${interaction.commandName} was found.`);
-		return;
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
 	}
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command.', ephemeral: true });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command.', ephemeral: true });
-		}
-	}
-});
-
-client.once(Events.ClientReady, () => {
-	console.log('[INFO] Logged in');
-});
+}
 
 // Log in to Discord with your client's token
 client.login(process.env.DISCORD_TOKEN);
